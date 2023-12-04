d5d2072bf3554477fa88b2780a277db560028018
weepfrog <housmanzach@gmail.com>
2023-12-04 13:47:11 -0500

    giving some references

---
+++ OutreachRobot2023/src/main/java/frc/robot/Drive.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+import frc.robot.subsystems.Subsystem;
+
+//here is a nice reference
+//https://docs.wpilib.org/en/stable/docs/zero-to-robot/step-4/creating-test-drivetrain-program-cpp-java.html
+// also code from our 2022
+//https://github.com/ligerbots/RapidReact2022/blob/main/src/main/java/frc/robot/subsystems/DriveTrain.java
+public class Drive extends Subsystem {
+    
+
+
+
+
+
+@Override
+public void zeroSensors() {
+}
+
+@Override
+public void outputToSmartDashboard() {
+    
+}
+
+@Override
+public void stop() {
+    
+}
+
+}
