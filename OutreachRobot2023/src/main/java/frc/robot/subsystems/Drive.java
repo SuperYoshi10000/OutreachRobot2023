a705363256f216d5b9228abc3529acddbc880631
weepfrog <housmanzach@gmail.com>
2023-12-07 20:13:17 -0500

    some drivetrain stuf

--- OutreachRobot2023/src/main/java/frc/robot/subsystems/Drive.java
+++ OutreachRobot2023/src/main/java/frc/robot/subsystems/Drive.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
 import java.security.PublicKey;
-
+import frc.robot.Constants;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //here is a nice reference
@@ -11,17 +12,32 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //https://github.com/ligerbots/RapidReact2022/blob/main/src/main/java/frc/robot/subsystems/DriveTrain.java
 public class Drive extends Subsystem {
     
-private final CANSparkMax m_motorRight;
-private final CANSparkMax m_motorLeft;
+private final CANSparkMax m_motorRight = new CANSparkMax(Constants.DriveMotorRightId, MotorType.kBrushless);
+private final CANSparkMax m_motorLeft = new CANSparkMax(Constants.DriveMotorLeftId, MotorType.kBrushless);
 
 private DifferentialDrive m_differentialDrive;
 
-public DriveTrain(){
+public Drive(){
 
+    
     m_differentialDrive = new DifferentialDrive(m_motorRight, m_motorLeft);
 
 }
 
+public double getRightSpeed(){
+    return -m_motorRight.get();
+}
+
+public double getLeftSpeed(){
+    return -m_motorLeft.get(); 
+
+}
+
+public void drive(double throttle, double rotate, boolean squaredInput) {
+    // SmartDashboard.putNumber("driveTrain/throttle", throttle);
+
+    m_differentialDrive.arcadeDrive(throttle, -rotate, squaredInput);
+}
 
 @Override
 public void zeroSensors() {
